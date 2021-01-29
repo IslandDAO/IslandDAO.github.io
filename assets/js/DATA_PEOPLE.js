4b48d05d9e573ec93354a0d7403ce894238ad9ee
Mars <email@genesis.re>
2021-01-29 00:08:56 +0000

    Testing

---
+++ assets/js/DATA_PEOPLE.js
@@ -0,0 +1,91 @@
+TEAM = [
+    {
+        name: "Mars Robertson",
+        image: "mars.jpg",
+        job: "Co-creator<br>Project Lead",
+        link: "https://marsrobertson.com",
+        twitter: "marsXrobertson",
+        linkedin: "marsrobertson/",
+        instagram: "marsrobertson/"
+    },
+    {
+        name: "Joanna Picetti",
+        image: "joanna.jpg",
+        job: "Chica Problematica<br>Politician, Visionary, Futurist",
+        link: "https://jp.network",
+        twitter: "joannapicetti",
+        linkedin: "joanna-picetti-she-her-hers-12a85010",
+        instagram: "joannapicetti",
+    },
+    {
+        name: "Camille Corti",
+        image: "camille.jpg",
+        job: "Creative Advisor<br>1st Committed Investor",
+        link: "http://blockchainhippy.io",
+        twitter: "blockchainhippy",
+        linkedin: "camille-corti-92268b155",
+        instagram: "camillecorti",
+    },
+    {
+        name: "Gamithra Marga",
+        image: "gamithra.jpg",
+        job: "On a mission<br>to fix the world",
+        link: "http://gamithra.com",
+        twitter: "gamithra_marga",
+        linkedin: "gamithra",
+        instagram: "gamithra",
+    },
+    {
+        name: "Dan Matthews",
+        image: "dan.jpg",
+        job: "Full stack extraordinaire<br>",
+        link: "https://cryptobuilders.io",
+        twitter: "LSDan_DeFi",
+        linkedin: "dandefi",
+        instagram: "",
+    },
+    {
+        name: "Join us",
+        image: "welcome.jpg",
+        job: "Join the <a href='http://basexisland.com/discord'>Discord chat</a><br>" +
+                "Book a call: <a href='https://calendly.com/marsxr/basex'>calendly.com/marsxr/basex</a>",
+        link: "",
+        twitter: "",
+        linkedin: "",
+        instagram: "",
+    }
+]
+
+ADVISORS = [
+    {
+        name: "Marc Winn",
+        image: "marc.jpg",
+        job: "Island Development Advisor<br>Co-founder of <a href='http://dandelion.gg/'>dandellion.gg</a>",
+        link: "https://marcwinn.com",
+        twitter: "marcwinn",
+        linkedin: "marcwinn/",
+        instagram: "marcwinn/"
+    },
+    {
+        name: "Julien Carbonnell",
+        image: "julien.jpg",
+        job: "Smart-City entrepreneur<br>PhD researcher",
+        link: "https://medium.com/@julien.carbonnell",
+        twitter: "Open_Pi",
+        linkedin: "juliencarbonnell",
+        instagram: "julien.carbonnell"
+    }
+
+];
+
+MEMERY = [
+    {
+        name: "Elon Musk",
+        image: "elon.jpg",
+        job: "Chief Meme Officer<br>",
+        link: "https://spacex.com",
+        twitter: "elonmusk",
+        linkedin: "",
+        instagram: "",
+    }
+]
---
+++ assets/js/main-angular.js
@@ -0,0 +1,74 @@
+
+var app = angular.module("app", ["ngRoute"]);
+
+app.config(function ($routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'pages/main.html',
+            controller: function($scope, $q) {
+                $scope.team = TEAM;
+                $scope.advisors = ADVISORS;
+                $scope.memery = MEMERY;
+
+                $scope.enable = async function() {
+                    await window.ethereum.enable();
+
+
+
+
+                    // provider = await new ethers.providers.Web3Provider(window.ethereum)
+                    // signer = await provider.getSigner();
+                    // $scope.address = (await provider.listAccounts())[0];
+                
+                    // assurance = await new ethers.Contract(AssuranceAddress, AssuranceABI, signer);
+                    // island    = await new ethers.Contract(IslandAddress,    IslandABI,    signer);
+            
+                    // let balance = await provider.getBalance($scope.address)
+                    // let balanceIsland = await island.balanceOf($scope.address);
+                    // let allowanceIsland = await island.allowance($scope.address, AssuranceAddress);
+                    
+                    // console.log(balance, balanceIsland.toString(),  allowanceIsland.toString());
+            
+                    // $scope.balance = ethers.utils.formatEther(balance.toString());
+                    // $scope.balanceIsland = ethers.utils.formatEther(balanceIsland.toString());
+                    // $scope.allowanceIsland = ethers.utils.formatEther(allowanceIsland.toString());
+
+                    $scope.$apply();
+                }
+            
+
+
+            }
+        })
+        .when('/bank', {
+            templateUrl: 'pages/bank.html',
+        })
+        .when('/trip', {
+            templateUrl: 'pages/trip.html',
+        })
+
+        .otherwise('/')
+});
+
+app.filter('unsafe', function($sce) { return $sce.trustAsHtml; });
+
+async function init() {
+
+    // let provider = new ethers.providers.InfuraProvider('ropsten');
+
+    await window.ethereum.enable();
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
+    account = (await provider.listAccounts())[0]
+
+
+    provider.getBalance(account).then((balance) => {
+        let etherString = ethers.utils.formatEther(balance);
+        console.log("Balance: " + etherString);
+    });
+
+
+
+}
+
+init();
